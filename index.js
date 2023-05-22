85d175d50f542aaa2b7d2ac18128300f1cd7670d
Ved Nakum <126437129+byteved@users.noreply.github.com>
2023-05-22 15:02:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //goofy ahh//
+(function changeSelect() {
+if (document.getElementById("dropBox").value = "1") {
+    document.getElementById("divText").innerhtml = "Michigan.Gov";
+} else if (document.getElementById("dropBox").value = "2") {
+    document.getElementById("divText").innerhtml = "Nps.gov";
+} else if (document.getElementById("dropBox").value = "3") {
+    document.getElementById("divText").innerhtml = "noa.gov";
+} else if (document.getElementById("dropBox").value = "4") {
+    document.getElementById("divText").innerhtml = "e";
+}
+});
